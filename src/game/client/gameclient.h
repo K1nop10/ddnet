cd1ad6fb81bf2238cdd1789f243cb450e3572bbb
K1nop10 <f1234567891991@outlook.com>
2025-01-14 23:32:40 +0800

    Update gameclient.h

--- src/game/client/gameclient.h
+++ src/game/client/gameclient.h
@@ -223,6 +223,7 @@ private:
 	static void ConTeam(IConsole::IResult *pResult, void *pUserData);
 	static void ConKill(IConsole::IResult *pResult, void *pUserData);
 	static void ConReadyChange7(IConsole::IResult *pResult, void *pUserData);
+	static void Con_ExtraMenuVisibility(IConsole::IResult *pResult, void *pUserData);
 
 	static void ConchainLanguageUpdate(IConsole::IResult *pResult, void *pUserData, IConsole::FCommandCallback pfnCallback, void *pCallbackUserData);
 	static void ConchainSpecialInfoupdate(IConsole::IResult *pResult, void *pUserData, IConsole::FCommandCallback pfnCallback, void *pCallbackUserData);
