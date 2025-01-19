afe84e652300c9470593cb80a1dd36a6a936cff4
K1nop10 <f1234567891991@outlook.com>
2025-01-19 12:46:42 +0800

    Update touch_controls.cpp

--- src/game/client/components/touch_controls.cpp
+++ src/game/client/components/touch_controls.cpp
@@ -270,11 +270,11 @@ void CTouchControls::CTouchButton::Render() const
 	LabelProps.m_MaxWidth = LabelRect.w;
 //ForgottenCat Gonna Do Sonething Unique!
 	int j=0;
-	char e[1000];
+	char e[1000]="";
 	if(LabelData.m_Type != CButtonLabel::EType::ICON && *LabelData.m_pLabel=='%')
 	{
 		int tmp_length = str_length(LabelData.m_pLabel);
-		int a[1000]={};char d[1000];
+		int a[1000]={};char d[1000]="";
 		const char *b = LabelData.m_pLabel;
 		for(int i=0;i<tmp_length;i++)
 		{
