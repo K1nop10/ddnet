b2e6855ebda9b5fcc4140ef389d5e98b8a473944
K1nop10 <f1234567891991@outlook.com>
2025-01-19 15:39:37 +0800

    Update touch_controls.h

--- src/game/client/components/touch_controls.h
+++ src/game/client/components/touch_controls.h
@@ -1180,6 +1180,7 @@ private:
 		EButtonShape m_Shape;
 		int m_BackgroundCorners; // only used with EButtonShape::RECT
 		float m_Rainbow=0.0f; //only used with EButtonShape::RAINBOW
+		std::chrono::nanoseconds m_RainbowTimer;
 
 		std::vector<CButtonVisibility> m_vVisibilities;
 		std::unique_ptr<CTouchButtonBehavior> m_pBehavior;
